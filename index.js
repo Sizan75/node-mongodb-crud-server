0ba90f729d205ad088b0178386530bcd350f7a8f
Sizan75 <sakiuzzaman26@gmail.com>
2022-10-31 12:05:12 +0600

    user data inserted in database

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app=express();
 const port= process.env.PORT || 5000;
+
+app.use(cors())
+app.use(express.json())
+
+
+
+const uri = "mongodb+srv://dbUser75:DrdWlkvqq9d2pyYQ@cluster0.bahxlpw.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run(){
+try{
+const userCollection= client.db('nodemongodbcrudserver').collection('users')
+const user={name: 'asid',email:'asid@gmail.com'}
+const result=await userCollection.insertOne(user)
+console.log(user)
+}
+finally{
+
+}
+}
+run().catch(error=> console.log(error))
+
 
 app.get('/',(req,res)=>{
     res.send('hello from node mongo db crud')
