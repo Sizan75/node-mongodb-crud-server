e9a4ac0c0538ae56870d23019a8fd9c067d98d85
Sizan75 <sakiuzzaman26@gmail.com>
2022-10-31 21:50:44 +0600

    user deleted from db function added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app=express();
 const port= process.env.PORT || 5000;
 
@@ -27,6 +27,14 @@ app.post('/users',async(req,res)=>{
     console.log(user)
     const result= await userCollection.insertOne(user)
     res.send(user)
+})
+
+app.delete('/users/:id',async(req,res)=>{
+    const id = req.params.id;
+    const query= {_id: ObjectId(id)}
+    const result=await userCollection.deleteOne(query)
+    console.log(result)
+    res.send(result)
 })
 }
 finally{
